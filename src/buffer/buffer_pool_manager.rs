7a5f5905f8dba352e2526a9468f030069e1d1c91
tkoizumi3 <takayuki.koizumi@gmail.com>
2026-03-30 16:49:02 -0400

    Initialize FrameHeader

--- src/buffer/buffer_pool_manager.rs
+++ src/buffer/buffer_pool_manager.rs
@@ -1,2 +1,20 @@
+use crate::constants::PAGE_SIZE;
+
+struct FrameHeader {
+    frame_id: usize,
+    pin_count: usize,
+    is_dirty: bool,
+    data: [u8; PAGE_SIZE],
+}
+
+impl FrameHeader {
+    fn new(frame_id: usize) -> Self {
+        Self {
+            frame_id,
+            pin_count: 0,
+            is_dirty: false,
+            data: [0u8; 4096],
+        }
+    }
+}
 struct BufferPoolManager {}
-struct FrameHeader {}
--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -1,3 +1,4 @@
+pub mod buffer_pool_manager;
 pub mod lru_k_replacer;
 
 pub enum AccessType {
