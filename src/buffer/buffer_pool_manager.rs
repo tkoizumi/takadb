562529e1da7083bbab909e12309988e2c1111a5e
tkoizumi3 <takayuki.koizumi@gmail.com>
2026-03-30 17:11:10 -0400

    Implement FrameHeader methods

--- src/buffer/buffer_pool_manager.rs
+++ src/buffer/buffer_pool_manager.rs
@@ -16,5 +16,16 @@ impl FrameHeader {
             data: [0u8; 4096],
         }
     }
+    fn get_data(&self) -> &[u8] {
+        &self.data
+    }
+    fn get_mut_data(&mut self) -> &mut [u8] {
+        &mut self.data
+    }
+    fn reset(&mut self) {
+        self.data.fill(0);
+        self.pin_count = 0;
+        self.is_dirty = false
+    }
 }
 struct BufferPoolManager {}
