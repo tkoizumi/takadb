6b46bc26b58397e7ce1c0a0f914b24a26bcdc041
tkoizumi3 <takayuki.koizumi@gmail.com>
2026-03-31 17:23:38 -0400

    Add constructors to page guards

--- src/storage/page/page_guard.rs
+++ src/storage/page/page_guard.rs
@@ -1,6 +1,6 @@
 use crate::buffer::buffer_pool_manager::{BufferPoolManager, FrameHeader};
 use crate::buffer::lru_k_replacer::LruKReplacer;
-use crate::storage::disk::disk_scheduler::DiskScheduler;
+use crate::storage::disk::disk_scheduler::{self, DiskScheduler};
 use std::sync::{Arc, Mutex};
 
 struct ReadPageGuard {
@@ -12,6 +12,25 @@ struct ReadPageGuard {
     is_valid: bool,
 }
 
+impl ReadPageGuard {
+    fn new(
+        page_id: usize,
+        frame: Arc<FrameHeader>,
+        replacer: Arc<LruKReplacer>,
+        bpm_latch: Arc<Mutex<BufferPoolManager>>,
+        disk_scheduler: Arc<DiskScheduler>,
+    ) -> Self {
+        Self {
+            page_id,
+            frame,
+            replacer,
+            bpm_latch,
+            disk_scheduler,
+            is_valid: false,
+        }
+    }
+}
+
 struct WritePageGuard {
     page_id: usize,
     frame: Arc<FrameHeader>,
@@ -19,4 +38,23 @@ struct WritePageGuard {
     bpm_latch: Arc<Mutex<BufferPoolManager>>,
     disk_scheduler: Arc<DiskScheduler>,
     is_valid: bool,
+}
+
+impl WritePageGuard {
+    fn new(
+        page_id: usize,
+        frame: Arc<FrameHeader>,
+        replacer: Arc<LruKReplacer>,
+        bpm_latch: Arc<Mutex<BufferPoolManager>>,
+        disk_scheduler: Arc<DiskScheduler>,
+    ) -> Self {
+        Self {
+            page_id,
+            frame,
+            replacer,
+            bpm_latch,
+            disk_scheduler,
+            is_valid: false,
+        }
+    }
 }
